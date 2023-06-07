6088f4d0e6f6f4976ac14a28849a5bb09000e3e6
hitom <hitomi.ito.slab@gmail.com>
2023-06-07 15:24:25 +0900

    farst

---
+++ src/main/java/com/example/puroduct1/Puroduct1Application.java
@@ -0,0 +1,13 @@
+package com.example.puroduct1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Puroduct1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Puroduct1Application.class, args);
+    }
+
+}
