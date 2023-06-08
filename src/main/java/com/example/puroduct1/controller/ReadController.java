f1e02e4264e21dcc5148e18abd01cc1d50561a51
hitom <hitomi.ito.slab@gmail.com>
2023-06-08 10:52:24 +0900

    read2終了！

--- src/main/java/com/example/puroduct1/controller/ReadController.java
+++ src/main/java/com/example/puroduct1/controller/ReadController.java
@@ -3,6 +3,7 @@ package com.example.puroduct1.controller;
 import com.example.puroduct1.service.ReadService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 //readコントローラー
@@ -24,7 +25,13 @@ public class ReadController {
 
     //商品一覧初期表示
     @RequestMapping(value = "/init")
-    public String init(){
+    //modelは引数として設定することで使用できるようになる
+    public String init(Model model){
+        //表示日付取得
+        String displayDate = this.service.getDisplayDate();
+        //画面に値を渡すためにモデルのaddAttributeメソッドを使用
+        //引数には値名と値を指定。今回はdisplayDateという名前で表示用日付を格納
+        model.addAttribute("displayDate",displayDate);
         return "itemList";
     }
 
--- src/main/java/com/example/puroduct1/service/ReadService.java
+++ src/main/java/com/example/puroduct1/service/ReadService.java
@@ -9,13 +9,14 @@ import java.util.Date;
 @Service
 public class ReadService {
     //表示用日付を取得。このメゾットではフォーマットした日付を文字列で返すので。戻り値はstring
+    //＠return表示用の日付
     public String getDisplayDate(){
         //システム日付を取得
         Date date = new Date();
 
         //yyyy年mm月dd日の形式に変換
         //yyyy年mm月dd日の形式に日付をフォーマットするため、SimpleDateFormatを作成します
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern: "yyyy年mm月dd日")
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy年MM月dd日");
         String displayDate = simpleDateFormat.format(date);
 
         //formatメソッドでフォーマットした日付を取得し、returnする
